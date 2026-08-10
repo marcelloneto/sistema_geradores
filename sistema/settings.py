3f3b91ca067405a868dad6855054573044e13390
Marcelo Neto <marcelo.neto@groupinsight.com.br>
2026-08-10 16:18:18 -0300

    adição da página de materiais

--- sistema/settings.py
+++ sistema/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
         "HOST": os.getenv("DB_HOST"),
         "PORT": os.getenv("DB_PORT", "5432"),
         "OPTIONS": {
-            "sslmode": "disable",
+            "sslmode": os.getenv("SSLMODE"),
         },
     }
 }
